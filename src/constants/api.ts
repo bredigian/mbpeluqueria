b31af18ef856e050a4af3d67304cc8efb7ba9c40
Gianluca Bredice - Nitro 5 <bredicegian@gmail.com>
2023-10-03 20:31:28 -0300

    [change] change type of env vars

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,2 +1,2 @@
-export const MONGODB_URI = process.env.NEXT_PUBLIC_MONGO_URI
-export const API_URL = process.env.NEXT_PUBLIC_URL_API
+export const API_URL: any = process.env.NEXT_PUBLIC_URL_API
+export const MONGODB_URI: any = process.env.NEXT_PUBLIC_MONGODB_URI
