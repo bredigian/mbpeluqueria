50a577cdfceab76205f8451fd1635b8ff1caf4aa
Gianluca Bredice - Nitro 5 <bredicegian@gmail.com>
2023-10-06 17:10:16 -0300

    [feature] add favicon and seo config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,19 @@ import { Toaster } from "sonner"
 
 const inter = Inter({ subsets: ["latin"] })
 
-export const metadata: Metadata = {
-  title: "MB Peluqueria",
+const metadata: Metadata = {
+  title: "MB Peluquería",
   description: "Reservá tu turno para cortarte el pelo acá mismo en instantes.",
+  keywords:
+    "peluquería, peluquero, pelo, cortar, cortes, cortes de pelo, masculino, niños, barba, barberia, barbero",
+  authors: [
+    {
+      name: "Gianluca Bredice Developer",
+      url: "https://devbredicegian.site",
+    },
+  ],
+  robots: "index",
+  applicationName: "MB Peluquería",
 }
 
 export default function RootLayout({
