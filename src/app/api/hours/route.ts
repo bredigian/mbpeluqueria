fdd2fc98722801183ee59e97b9b34514d2879da4
Gianluca Bredice - Nitro 5 <bredicegian@gmail.com>
2023-10-06 21:08:55 -0300

    [fix] trycatch on hours endpoint

--- src/app/api/hours/route.ts
+++ src/app/api/hours/route.ts
@@ -5,14 +5,26 @@ import { connectDB } from "@/utils/mongoose"
 export const GET = async () => {
   await connectDB()
 
-  const workHours = await WorkHour.find()
-
-  return NextResponse.json(
-    {
-      workHours,
-    },
-    {
-      status: 200,
-    }
-  )
+  try {
+    const workHours = await WorkHour.find()
+    return NextResponse.json(
+      {
+        workHours,
+      },
+      {
+        status: 200,
+      }
+    )
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Ocurrió un error al obtener los horarios",
+        ok: false,
+      },
+      {
+        status: 400,
+        statusText: "Bad Request",
+      }
+    )
+  }
 }
