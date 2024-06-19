d2321e0aafed8ad9fccdd24956cea8ee788bf2aa
Gianluca Bredice <bredicegian@gmail.com>
2024-06-18 23:02:47 -0300

    clean: clean project of obsolete files

--- src/components/old/Modal.tsx
+++
@@ -1,9 +0,0 @@
-const Modal = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className='bg-dark-bold-transparent fixed top-0 grid h-screen w-screen place-items-center backdrop-blur-[2px]'>
-      {children}
-    </div>
-  );
-};
-
-export default Modal;
--- src/types/old/date.types.tsx
+++
@@ -1,7 +0,0 @@
-export interface Date {
-  day: number;
-  month: number;
-  year: number;
-  dateString: string;
-  dayWeek: number;
-}
