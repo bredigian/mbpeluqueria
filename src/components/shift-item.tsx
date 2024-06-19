ce16231b30153f2291ee1f91a679f5a073711318
Gianluca Bredice <bredicegian@gmail.com>
2024-06-19 12:11:26 -0300

    refactor: code refactoring and add dropdown menu component

---
+++ src/components/shift-item.tsx
@@ -0,0 +1,55 @@
+import { Card, CardContent } from './ui/card';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+
+import { Button } from './ui/button';
+import { DotsHorizontalIcon } from '@radix-ui/react-icons';
+import { IShift } from '@/types/shifts.types';
+
+type Props = {
+  data: IShift;
+};
+
+export default function ShiftItem({ data }: Props) {
+  const date = new Date(data.timestamp);
+
+  return (
+    <li>
+      <Card>
+        <CardContent className='flex w-full items-start justify-between'>
+          <div className='flex flex-col gap-2'>
+            <small className='text-base opacity-75'>
+              {date.toLocaleDateString('es-AR')}
+            </small>
+            <span className='text-3xl font-medium'>
+              {date
+                .toLocaleTimeString('es-AR', { hour12: false })
+                .substring(0, 5)}
+            </span>
+          </div>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button size='icon' variant='outline'>
+                <DotsHorizontalIcon />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>Opciones</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem>Cancelar</DropdownMenuItem>
+              </DropdownMenuGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </CardContent>
+      </Card>
+    </li>
+  );
+}
--- src/components/shifts-container.tsx
+++ src/components/shifts-container.tsx
@@ -1,8 +1,7 @@
-import { Card, CardContent } from './ui/card';
-
 import { Button } from './ui/button';
 import CalendarAddIcon from './icons/calendar-add-icon';
 import { IShift } from '@/types/shifts.types';
+import ShiftItem from './shift-item';
 import { Subtitle } from './ui/subtitle';
 import { TResponse } from '@/types/responses.types';
 import { cookies } from 'next/headers';
@@ -33,27 +32,9 @@ export default async function ShiftsContainer() {
           </aside>
           <ul className='flex flex-col gap-6'>
             {(shifts as IShift[]).length > 0 ? (
-              (shifts as IShift[]).map((shift) => {
-                const date = new Date(shift.timestamp);
-                return (
-                  <li key={shift.id}>
-                    <Card>
-                      <CardContent className='flex w-full items-start justify-between'>
-                        <div className='flex flex-col gap-2'>
-                          <small className='text-base opacity-75'>
-                            {date.toLocaleDateString('es-AR')}
-                          </small>
-                          <span className='text-3xl font-medium'>
-                            {date
-                              .toLocaleTimeString('es-AR', { hour12: false })
-                              .substring(0, 5)}
-                          </span>
-                        </div>
-                      </CardContent>
-                    </Card>
-                  </li>
-                );
-              })
+              (shifts as IShift[]).map((shift) => (
+                <ShiftItem key={shift.id} data={shift} />
+              ))
             ) : (
               <span>No tenés turnos agendados.</span>
             )}
