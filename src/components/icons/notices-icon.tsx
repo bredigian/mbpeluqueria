9fe430f340d8c736fa7e0195a25b3c5ed1879caa
Gianluca Bredice <bredicegian@gmail.com>
2024-06-25 18:15:23 -0300

    fix: notices icon stroke width

--- src/components/icons/notices-icon.tsx
+++ src/components/icons/notices-icon.tsx
@@ -11,7 +11,7 @@ export default function NoticesIcon({ size, color }: Props) {
       width={size}
       height={size}
       viewBox='0 0 24 24'
-      strokeWidth='1.5'
+      strokeWidth='2'
       stroke={color}
       fill='none'
       strokeLinecap='round'
