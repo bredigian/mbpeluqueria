07089884a99127b9cf31cc07e9540c5bbbf879ce
Gianluca Bredice <bredicegian@gmail.com>
2024-07-02 10:50:14 -0300

    feature: add link to WhatsApp on shift item rendered for admin user

--- src/components/shift-item.tsx
+++ src/components/shift-item.tsx
@@ -5,6 +5,7 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuGroup,
+  DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
@@ -14,6 +15,7 @@ import { Button } from './ui/button';
 import { CancelShiftDialog } from './shift-dialog';
 import { DotsHorizontalIcon } from '@radix-ui/react-icons';
 import { IShift } from '@/types/shifts.types';
+import Link from 'next/link';
 import { Skeleton } from './ui/skeleton';
 import { cn } from '@/lib/utils';
 
@@ -93,6 +95,23 @@ export function ShiftItem({ data, isForAdmin }: Props) {
                 <DropdownMenuLabel>Opciones</DropdownMenuLabel>
                 <DropdownMenuSeparator />
                 <DropdownMenuGroup>
+                  {isForAdmin && (
+                    <Link
+                      href={`https://api.whatsapp.com/send?phone=${data.user?.phone_number}&text=¡Hola%20👋!%0ATe%20recuerdo%20que%20tenés%20un%20turno%20el%20día%20*${dateToString.charAt(0).toUpperCase() + dateToString.slice(1)}*%20a%20las%20*${date
+                        .toLocaleTimeString('es-AR', {
+                          timeZone: 'America/Argentina/Buenos_Aires',
+                          hour12: false,
+                        })
+                        .substring(
+                          0,
+                          5,
+                        )}hs*%0ASi%20deseás%20cancelar%20el%20turno,%20podrás%20solicitarlo%20desde%20la%20página%20de%20turnos.%0A¡Muchas%20gracias,%20te%20espero!%20💈`}
+                    >
+                      <DropdownMenuItem className='flex w-full justify-start gap-2 rounded-md px-3 py-2 font-medium duration-200 hover:bg-accent'>
+                        Contactar por WhatsApp
+                      </DropdownMenuItem>
+                    </Link>
+                  )}
                   <CancelShiftDialog
                     id={data.id as string}
                     user_name={data.user?.name as string}
